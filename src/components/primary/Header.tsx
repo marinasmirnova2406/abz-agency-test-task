cc79f4eacec7b30205b668cae7741f41aba3c146
marinasmirnova2406 <marinasmirnova2406@gmail.com>
2025-05-03 18:29:48 +0200

    Add header and breakpoints

--- src/components/primary/Header.tsx
+++ src/components/primary/Header.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+// Images
+import logo from "../../assets/images/logo.svg";
+//Components
+import Button from "../common/Button";
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="header__container">
-        Header
+      <div className="header__top-color-block"></div>
+      <div className="header__content container">
+        <img src={logo} alt="TestTask Logo" className="header__logo" />
+        <nav className="header__nav">
+          <Button>Users</Button>
+          <Button>Sign up</Button>
+        </nav>
       </div>
     </header>
   );
